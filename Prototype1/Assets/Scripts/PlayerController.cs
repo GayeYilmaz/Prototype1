0b60de52c6b2fdbc4b309f70e63117ddf8217d27
Gaye Yılmaz <gaye3545@gmail.com>
2022-03-16 18:30:11 +0300

    Location of camera set

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +15,8 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //We'll move the vehicle forward
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
     }
 }
